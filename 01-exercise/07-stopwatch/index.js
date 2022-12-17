2ac1b56aaf84fcd9609cae8c0dccf240d7a3e437
dohaelee <dev.dohaelee@gmail.com>
2022-12-17 14:53:28 +0900

    1-7 예제 정답 파일 추가

--- 01-exercise/07-stopwatch/index.js
+++ 01-exercise/07-stopwatch/index.js
@@ -1,2 +1,56 @@
 // 💡 https://github.com/erumcoding/js-101-part-1
-// 👉 이 아래 코드를 작성하세요.
+
+const startButton = document.querySelector('.start');
+const stopButton = document.querySelector('.stop');
+const resetButton = document.querySelector('.reset');
+
+let timerId;
+
+let [msec, sec, min] = [0, 0, 0];
+
+const displayTimer = () => {
+  const time = document.querySelector('.time');
+
+  const fMin = min < 10 ? `0${min}` : min;
+  const fSec = sec < 10 ? `0${sec}` : sec;
+  const fMsec = msec < 10 ? `0${msec}` : msec;
+
+  time.innerText = `${fMin} : ${fSec} : ${fMsec}`;
+};
+
+const timer = () => {
+  msec++;
+
+  if (msec === 100) {
+    msec = 0;
+    sec++;
+
+    if (sec === 60) {
+      sec = 0;
+      min++;
+    }
+  }
+
+  displayTimer();
+};
+
+const start = () => {
+  // Start 버튼 연타해도 계속해서 타이머가 생성되지 않게끔 조건문 추가
+  if (!timerId) {
+    timerId = setInterval(timer, 10);
+  }
+};
+
+const stop = () => {
+  clearInterval(timerId);
+};
+
+const reset = () => {
+  stop();
+  [msec, sec, min] = [0, 0, 0];
+  displayTimer();
+};
+
+startButton.addEventListener('click', start);
+stopButton.addEventListener('click', stop);
+resetButton.addEventListener('click', reset);
