69dd1cb65a1b806c80df41e77edc564bcb17d73b
dohaelee <dev.dohaelee@gmail.com>
2022-12-17 14:49:56 +0900

    1-4 예제 정답 파일 추가

--- 01-exercise/04-image-slider/index.js
+++ 01-exercise/04-image-slider/index.js
@@ -1,2 +1,39 @@
 // 💡 https://github.com/erumcoding/js-101-part-1
-// 👉 이 아래 코드를 작성하세요.
+
+const images = document.querySelectorAll('.item');
+const prevButton = document.querySelector('.prev');
+const nextButton = document.querySelector('.next');
+
+let index = 0;
+let lastIndex = images.length - 1;
+
+const updateImage = () => {
+  images.forEach((img) => {
+    img.classList.remove('show');
+  });
+
+  images[index].classList.add('show');
+};
+
+const moveToNext = () => {
+  if (index === lastIndex) {
+    index = 0;
+  } else {
+    index++;
+  }
+
+  updateImage();
+};
+
+const moveToPrev = () => {
+  if (index === 0) {
+    index = lastIndex;
+  } else {
+    index--;
+  }
+
+  updateImage();
+};
+
+prevButton.addEventListener('click', moveToPrev);
+nextButton.addEventListener('click', moveToNext);
