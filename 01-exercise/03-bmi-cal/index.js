c3e8a33358ad3654e838319df733759086fa9c9c
dohaelee <dev.dohaelee@gmail.com>
2022-12-17 14:48:57 +0900

    1-3 예제 정답 파일 추가

--- 01-exercise/03-bmi-cal/index.js
+++ 01-exercise/03-bmi-cal/index.js
@@ -1,2 +1,46 @@
 // 💡 https://github.com/erumcoding/js-101-part-1
-// 👉 이 아래 코드를 작성하세요.
+
+const form = document.querySelector('form');
+
+const display = (bmi) => {
+  const result = document.querySelector('.result');
+  let group;
+
+  if (bmi >= 30.0) {
+    group = '중등도비만';
+  } else if (bmi >= 25.0) {
+    group = '경도비만';
+  } else if (bmi >= 23.0) {
+    group = '과체중';
+  } else if (bmi >= 18.5) {
+    group = '정상';
+  } else {
+    group = '저체중';
+  }
+
+  result.innerText = `${bmi} → ${group} 입니다.`;
+};
+
+const calculate = (weight, height) => {
+  // 키 170㎝에 몸무게 73kg이면, 계산식 : 73 / (1.7 × 1.7)
+  return (weight / (height * height)).toFixed(1);
+};
+
+const formHandler = (event) => {
+  event.preventDefault();
+
+  const heightInput = document.querySelector('#height');
+  const weightInput = document.querySelector('#weight');
+
+  if (weightInput.value !== '' && heightInput.value !== '') {
+    const weight = weightInput.value;
+    const height = heightInput.value / 100;
+    const bmi = calculate(weight, height);
+
+    display(bmi);
+    heightInput.value = '';
+    weightInput.value = '';
+  }
+};
+
+form.addEventListener('submit', formHandler);
