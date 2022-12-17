65ddb9476f55808998b0c5fefeda77d266dd8cd1
dohaelee <dev.dohaelee@gmail.com>
2022-12-17 14:59:07 +0900

    3 예제 정답 파일 추가

--- 03-canvas/01-drawing-board/index.js
+++ 03-canvas/01-drawing-board/index.js
@@ -1,2 +1,67 @@
 // 💡 https://github.com/erumcoding/js-101-part-1
-// 👉 이 아래 코드를 작성하세요.
+
+const canvas = document.querySelector('canvas');
+const color = document.querySelector('#color');
+const width = document.querySelector('#width');
+const clear = document.querySelector('.clear');
+const save = document.querySelector('.save');
+const ctx = canvas.getContext('2d');
+
+ctx.fillStyle = 'white';
+ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+let isPainting = false;
+let lineWidth = 5;
+
+save.addEventListener('click', () => {
+  canvas.toBlob((blob) => {
+    const a = document.createElement('a');
+    const body = document.querySelector('body');
+
+    a.href = URL.createObjectURL(blob);
+    a.download = 'erumcoding.jpg';
+
+    body.append(a);
+    a.click();
+    a.remove();
+  });
+});
+
+clear.addEventListener('click', () => {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+});
+
+width.addEventListener('change', (event) => {
+  lineWidth = event.target.value;
+});
+
+color.addEventListener('change', (event) => {
+  console.log(event.target.value);
+  ctx.strokeStyle = event.target.value;
+});
+
+canvas.addEventListener('mouseout', () => {
+  isPainting = false;
+});
+
+canvas.addEventListener('mousemove', (event) => {
+  if (!isPainting) {
+    return;
+  }
+
+  ctx.lineWidth = lineWidth;
+  ctx.lineCap = 'round';
+
+  ctx.lineTo(event.offsetX, event.offsetY);
+  ctx.stroke();
+});
+
+canvas.addEventListener('mouseup', () => {
+  isPainting = false;
+});
+
+canvas.addEventListener('mousedown', (event) => {
+  isPainting = true;
+  ctx.beginPath();
+  ctx.moveTo(event.offsetX, event.offsetY);
+});
--- 03-canvas/02-meme-generator/index.js
+++ 03-canvas/02-meme-generator/index.js
@@ -1,2 +1,75 @@
 // 💡 https://github.com/erumcoding/js-101-part-1
-// 👉 이 아래 코드를 작성하세요.
+
+const canvas = document.querySelector('canvas');
+const imageFile = document.querySelector('#image-file');
+const textInputs = document.querySelectorAll('.text');
+const topTextInput = document.querySelector('#top-text');
+const bottomTextInput = document.querySelector('#bottom-text');
+const ctx = canvas.getContext('2d');
+
+let image;
+let width;
+let height;
+
+let topText = '';
+let bottomText = '';
+
+const drawText = () => {
+  const offsetY = height / 20;
+  const fontSize = width / 10;
+
+  ctx.font = `${fontSize}px sans-serif`;
+  ctx.fillStyle = 'white';
+  ctx.textAlign = 'center';
+  ctx.strokeStyle = 'black';
+  ctx.lineWidth = fontSize / 5;
+  ctx.lineJoin = 'round';
+
+  ctx.textBaseline = 'top';
+  ctx.strokeText(topText, width / 2, offsetY);
+  ctx.fillText(topText, width / 2, offsetY);
+
+  ctx.textBaseline = 'bottom';
+  ctx.strokeText(bottomText, width / 2, height - offsetY);
+  ctx.fillText(bottomText, width / 2, height - offsetY);
+};
+
+const updateTopText = (event) => {
+  topText = event.target.value;
+  drawText();
+};
+
+const updateBottomText = (event) => {
+  bottomText = event.target.value;
+  drawText();
+};
+
+const showInputs = () => {
+  textInputs.forEach((input) => {
+    input.style.display = 'flex';
+  });
+};
+
+const uploadImage = () => {
+  width = image.width;
+  height = image.height;
+
+  canvas.width = width;
+  canvas.height = height;
+
+  ctx.drawImage(image, 0, 0);
+  showInputs();
+  drawText();
+};
+
+const createImage = (event) => {
+  const imageUrl = URL.createObjectURL(event.target.files[0]);
+
+  image = document.createElement('img');
+  image.src = imageUrl;
+  image.addEventListener('load', uploadImage);
+};
+
+imageFile.addEventListener('change', createImage);
+topTextInput.addEventListener('change', updateTopText);
+bottomTextInput.addEventListener('change', updateBottomText);
