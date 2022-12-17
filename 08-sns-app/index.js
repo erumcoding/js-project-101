4eca90df3a3988d9012423c0e7f39ab9bf33c6ce
dohaelee <dev.dohaelee@gmail.com>
2022-12-17 15:10:54 +0900

    8 예제 정답 파일 추가

--- 08-sns-app/index.js
+++ 08-sns-app/index.js
@@ -1,2 +1,51 @@
 // 💡 https://github.com/erumcoding/js-101-part-1
-// 👉 이 아래 코드를 작성하세요.
+
+const feed = document.querySelector('.feed');
+
+const getUserById = (id) => {
+  const URL = `https://jsonplaceholder.typicode.com/users/${id}`;
+
+  return fetch(URL)
+    .then((response) => response.json())
+    .then((data) => data);
+};
+
+const createPost = async (post) => {
+  const wrap = document.createElement('div');
+  const user = document.createElement('a');
+  const article = document.createElement('div');
+
+  wrap.className = 'post';
+  user.className = 'user';
+  article.className = 'article';
+
+  wrap.id = post.id;
+  article.innerText = post.body;
+
+  const userInfo = await getUserById(post.userId);
+  user.innerText = `@${userInfo.username}`;
+  user.href = './user.html';
+
+  user.addEventListener('click', () => {
+    localStorage.setItem('userId', post.userId);
+  });
+
+  wrap.append(user, article);
+  feed.append(wrap);
+};
+
+const getAllPosts = () => {
+  const URL = 'https://jsonplaceholder.typicode.com/posts';
+
+  fetch(URL)
+    .then((response) => response.json())
+    .then((data) => {
+      data.forEach((post) => {
+        createPost(post);
+      });
+    });
+};
+
+if (feed) {
+  getAllPosts();
+}
--- 08-sns-app/user.js
+++ 08-sns-app/user.js
@@ -1,2 +1,22 @@
 // 💡 https://github.com/erumcoding/js-101-part-1
-// 👉 이 아래 코드를 작성하세요.
+
+const updateProfile = (userData) => {
+  const name = document.querySelector('.name');
+  const email = document.querySelector('.email');
+  const website = document.querySelector('.website');
+
+  name.innerText = `${userData.name} (@${userData.username})`;
+  email.innerText = userData.email;
+  email.href = `mailto:${userData.email}`;
+  website.innerText = userData.website;
+  website.href = `http://${userData.website}`;
+  website.target = '_blank';
+};
+
+const loadUserProfile = async () => {
+  const userId = localStorage.getItem('userId');
+  const userData = await getUserById(userId);
+  updateProfile(userData);
+};
+
+loadUserProfile();
